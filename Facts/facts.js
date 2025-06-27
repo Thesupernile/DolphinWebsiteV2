b96a85233cd257985a3ea7147308d60d7fd6ef52
Thesupernile <thesupernile6@gmail.com>
2025-06-27 17:18:54 +0100

    Continued work on getting the facts page working. Struggled with the syntax for file reading in js

--- Facts/facts.js
+++ Facts/facts.js
@@ -1,24 +1,37 @@
 function showInfoScreen(textOption){
     // 0 is bottlenose, 1 is orca, 2 is spinner and 3 is amazon river dolphin
-    let habitatFilepath = "InfoParagraphs/Habitat-${textOption}.txt";
-    let dietFilepath = "InfoParagraphs/Diet-Bottlenose.txt";
-    let additionalFactFilepath = "InfoParagraphs/AdditionalFacts-Bottlenose";
+    let habitatFilepath = "InfoParagraphs/Habitat-" + textOption + ".txt";
+    let dietFilepath = "InfoParagraphs/Diet-" + textOption + ".txt";
+    let additionalFactFilepath = "InfoParagraphs/AdditionalFacts-" + textOption + ".txt";
+
+    let habitatText;
+    let dietText;
+    let additionalText;
+
+    var reader = new FileReader();
+
+    habitatText = reader.readAsText(habitatFilepath);
+
+    document.getElementById("habitatInfo").innerHTML = habitatText;
+    document.getElementById("dietInfo").innerHTML = dietText;
+    document.getElementById("additionalInfo").innerHTML = additionalText;
+
 }
 
 
 function bottlenoseButtonClicked(){
-    showInfoScreen("bottlenose");
+    showInfoScreen("Bottlenose");
 }
 
 function orcaButtonClicked(){
-    showInfoScreen("orca");
+    showInfoScreen("Orca");
 }
 
 function spinnerButtonClicked(){
-    showInfoScreen("spinner");
+    showInfoScreen("Spinner");
 }
 
 function amazonRiverButtonClicked(){
-    showInfoScreen("amazonRiver");
+    showInfoScreen("AmazonRiver");
 }
 
